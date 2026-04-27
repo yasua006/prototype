a8193988a067a9e7523858f977be9dd70b97b247
yasua006 <yasua006@osloskolen.no>
2026-04-27 17:34:18 +0200

    feat(bytte mapper): kun bytt til hele session
    Bytte "current" mappe i alle cwd "parameters" for å kunne gjøre det samme inn i den byttet mappe (legg, fjern,
    eller bytt en mappe)

--- modules/run_cmd.py
+++ modules/run_cmd.py
@@ -2,5 +2,6 @@ import subprocess as sp
 from subprocess import CompletedProcess
 
 
-def run_cmd(cmd_to_run) -> CompletedProcess[str]:
-    return sp.run([cmd_to_run], text=True, shell=True)
+def run_cmd(cmd_to_run, working_dir: str | None) -> CompletedProcess[str]:
+    return sp.run([cmd_to_run], text=True, shell=True,
+        cwd=working_dir)
